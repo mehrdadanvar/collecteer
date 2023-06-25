67db74ebcf25f1409dc941ec0457dfac15bf7574
mehrdad.anvar <mehrdad.anvar@gmail.com>
2023-06-25 10:44:17 -0700

    updated shikiconfig for syntax highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,24 +3,21 @@ import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import mdx from "@astrojs/mdx";
 import cityLights from "./city-lights.json";
+import Nord from "./Nord.json";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue(), mdx()],
   markdown: {
-    // syntaxHighlight: "shiki",
-
     shikiConfig: {
-      theme: {
-        name: "city-lights",
-        type: "dark",
-        settings: cityLights.tokenColors,
-      },
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: Nord,
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
     },
-    // Add custom languages
-    // Note: Shiki has countless langs built-in, including .astro!
-    // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-    // langs: ["js", "python"],
-    // // Enable word wrap to prevent horizontal scrolling
-    // wrap: true,
   },
 });
