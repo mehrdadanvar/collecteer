35b5c71b67a386142137a37b439d74c4e7fb33d5
mehrdad.anvar <mehrdad.anvar@gmail.com>
2023-06-26 04:16:40 -0700

    updated configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
@@ -12,6 +10,7 @@ module.exports = {
         // FiraMono: ["fira mono"],
         display: ["Russo One", "sans-serif"],
         "Aneck-Gurmukhi": ["'Anek Gurmukhi'"],
+        Inter: ["'Inter'"],
       },
     },
   },
